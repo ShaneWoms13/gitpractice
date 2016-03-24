00d89e36a123550a8ffd23a9d76e6f992aab1e0a
shanewoms13 <15Womels7081@pit.edu>
2016-03-24 18:11:20 +0000

    centered content

--- main.js
+++ main.js
@@ -8,3 +8,18 @@ document.body.appendChild(content);
 
 //put some words inside the content div
 content.innerHTML = "hello world";
+
+//increase text size of content
+content.style.fontSize = "50px";
+
+//change background
+content.style.backgroundColor = "teal";
+
+//half screen width
+content.style.width = "50%";
+
+//center the text within the content div
+content.style.textAlign = "center";
+
+//center the content div
+content.style.margin = "10% auto";
