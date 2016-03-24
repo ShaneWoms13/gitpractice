d47aff82d3640241672f023acf1edd908d6eabfa
shanewoms13 <15Womels7081@pit.edu>
2016-03-24 17:52:37 +0000

    made content

---
+++ main.js
@@ -0,0 +1,10 @@
+//let's build the html elements
+
+//create a div
+var content = document.createElement("div");
+
+//place the content div in the body
+document.body.appendChild(content);
+
+//put some words inside the content div
+content.innerHTML = "hello world";
